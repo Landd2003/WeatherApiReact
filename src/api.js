9d7dbc572ee5940aa8bc8589ad5047c5b5186079
Landd2003 <landiperdodaj@gmail.com>
2024-08-02 20:04:23 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,18 @@
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': '42f015ff49msh6e53e2068aca6b9p1ecbb4jsnaf7a59c88b33',
+        'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "77ad4c10a4438bc48a49227bd441c681";
+
+
+
+
